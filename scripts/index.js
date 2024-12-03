bed7302c4a2e89736c62cbda75659e5b96e4c881
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-03 14:27:39 +0300

    feat: join all DOMContentLoaded

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,14 @@
 const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
-
 let avalibleCards = [];
-document.addEventListener('DOMContentLoaded', function () {
-    initialCards.forEach(card => {
-        avalibleCards.push(card);
-    });
-});
 
 function addCard(nameValue, linkValue) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = nameValue;
     cardElement.querySelector('.card__image').src = linkValue;
+
     const resetButton = cardElement.querySelector('.card__delete-button');
     resetButton.addEventListener('click', function () {
         cardElement.remove();
@@ -24,15 +19,17 @@ function addCard(nameValue, linkValue) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    initialCards.forEach(card => {
+        avalibleCards.push(card);
+    });
+
     for (let i = 0; i < avalibleCards.length; i++) {
         const card = avalibleCards[i];
         addCard(card.name, card.link);
         avalibleCards.splice(i, 1);
         i--;
     }
-});
 
-document.addEventListener('DOMContentLoaded', function () {
     addButton.addEventListener('click', function () {
         if (avalibleCards.length > 0){
             const obj = avalibleCards[0];
