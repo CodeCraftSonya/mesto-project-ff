3a616550946b0ef42d93281430b40e3717d0ec21
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-03 14:18:39 +0300

    feat: add logic for the appearance of new cards

--- scripts/index.js
+++ scripts/index.js
@@ -2,44 +2,46 @@ const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
 
+let avalibleCards = [];
+document.addEventListener('DOMContentLoaded', function () {
+    initialCards.forEach(card => {
+        avalibleCards.push(card);
+    });
+});
 
 function addCard(nameValue, linkValue) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    console.log(cardElement);
-
     cardElement.querySelector('.card__title').textContent = nameValue;
     cardElement.querySelector('.card__image').src = linkValue;
     const resetButton = cardElement.querySelector('.card__delete-button');
     resetButton.addEventListener('click', function () {
         cardElement.remove();
+        avalibleCards.push({ name: nameValue, link: linkValue });
     });
 
     cardsContainer.append(cardElement);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    initialCards.forEach(card => {
+    for (let i = 0; i < avalibleCards.length; i++) {
+        const card = avalibleCards[i];
         addCard(card.name, card.link);
-    });
-
+        avalibleCards.splice(i, 1);
+        i--;
+    }
 });
-
 
 document.addEventListener('DOMContentLoaded', function () {
     addButton.addEventListener('click', function () {
-        console.log(document.querySelectorAll('.card').length);
-        let currentIndex = document.querySelectorAll('.card').length;
-        if (currentIndex < initialCards.length) {
-            const obj = initialCards[currentIndex]; // Берём объект из массива
-            const name = obj.name; // Извлекаем значение свойства name
-            const link = obj.link; // Получаем текущий объект
-            addCard(name, link); // Передаём name и link в функцию addCard
-            currentIndex++; // Увеличиваем индекс для следующего объекта
-        } else {
+        if (avalibleCards.length > 0){
+            const obj = avalibleCards[0];
+            const name = obj.name;
+            const link = obj.link;
+            addCard(name, link);
+            avalibleCards.splice(0, 1);
         }
     });
-
 });
 
 
