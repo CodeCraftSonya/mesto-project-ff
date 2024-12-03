4ab6144da24ee6c6a51c2cd43bd497c11bf33872
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-03 13:13:58 +0300

    feat: add additing and delete for cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,47 @@
-// @todo: Темплейт карточки
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
+const addButton = container.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function addCard(nameValue, linkValue) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    console.log(cardElement);
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__title').textContent = nameValue;
+    cardElement.querySelector('.card__image').src = linkValue;
+    const resetButton = cardElement.querySelector('.card__delete-button');
+    resetButton.addEventListener('click', function () {
+        cardElement.remove();
+    });
 
-// @todo: Вывести карточки на страницу
+    cardsContainer.append(cardElement);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    initialCards.forEach(card => {
+        addCard(card.name, card.link);
+    });
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    addButton.addEventListener('click', function () {
+        console.log(document.querySelectorAll('.card').length);
+        let currentIndex = document.querySelectorAll('.card').length;
+        if (currentIndex < initialCards.length) {
+            const obj = initialCards[currentIndex]; // Берём объект из массива
+            const name = obj.name; // Извлекаем значение свойства name
+            const link = obj.link; // Получаем текущий объект
+            addCard(name, link); // Передаём name и link в функцию addCard
+            currentIndex++; // Увеличиваем индекс для следующего объекта
+        } else {
+        }
+    });
+
+});
+
+
+
+
