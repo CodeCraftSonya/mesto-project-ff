80ef31e326d2efce85ac661498d9cdc95d053964
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-05 10:17:28 +0300

    fix: remove the double element definition

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,10 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(nameValue, linkValue, removeCardCallback) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
     cardElement.querySelector('.card__title').textContent = nameValue;
-    cardElement.querySelector('.card__image').src = linkValue;
-    cardElement.querySelector('.card__image').alt = nameValue;
+    cardImage.src = linkValue;
+    cardImage.alt = nameValue;
 
     const removeButton = cardElement.querySelector('.card__delete-button');
     removeButton.addEventListener('click', function () {
